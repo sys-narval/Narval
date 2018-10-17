f1aed459925e987eb1fe4cd309b2ce211643ab1c
antonio quesada <jose17971@outlook.es>
2018-10-16 21:27:44 -0600

    categorias

--- api/controllers/view-inventario.js
+++ api/controllers/view-inventario.js
@@ -18,48 +18,8 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     let modeloI= {
-      tipos:['TRUSS','Cables','Mobiliario','Herramientas'],
+      tipos:['TRUSS','Cables','Mobiliario','Herramientas/otros','Cortinaje','Tarimas'],
       unidadMedida: ['metros', 'unidades'],
-      /*articulos: [{
-        id:'T001',
-        descripcion : '0.25mts',
-        cantidadLibre: 10,
-        cantidadUso: 0,
-        cantidadDanado: 0,
-        cantidadTotal: 10,
-        tipo: 'TRUSS',
-        precio: 1000
-      },
-    {
-      id:'C001',
-      descripcion : 'Conectores de 3 Vias',
-      cantidadLibre: 1,
-      cantidadUso: 0,
-      cantidadDanado: 0,
-      cantidadTotal: 1,
-      tipo: "Cables",
-      precio: 2000
-    },
-  {
-    id:'M001',
-    descripcion : 'Sillón Blanco 1pax',
-      cantidadLibre: 2,
-      cantidadUso: 0,
-      cantidadDanado: 0,
-      cantidadTotal: 1,
-      tipo: 'Mobiliario',
-      precio: 2000
-  },
-  {
-    id:'H001',
-    descripcion : 'Maquina de Humo',
-    cantidadLibre: 1,
-    cantidadUso: 0,
-    cantidadDanado: 0,
-    cantidadTotal: 1,
-    tipo: 'Herramientas',
-    precio: 2000
-  }],*/
   articuloNuevo: {}
     };
 
