57f50442941cb74097ae2004a1c3c0b855b1b6d2
antonio17971 <jose.quesada.espinoza@est.una.ac.cr>
2018-10-24 10:59:19 -0600

    mejoraras de validacion

--- api/controllers/view-inventario.js
+++ api/controllers/view-inventario.js
@@ -20,8 +20,12 @@ module.exports = {
     let modelo= { //Retorna los datos necesarios
       a_categorias:['Truss','Cables','Mobiliario','Herramientas/otros','Cortinaje','Tarimas'], //Arreglo con las categorias de los articulos
       a_unidadMedida: ['metros', 'unidades'], //Arreglo con las uniades de medida
-      l_errorId : "El ID es requerido",
-      l_errorDescripcion : "La descripción es requerida", //Variables con las validaciones de los errores principales de un form
+      l_errorId : 'El ID es requerido',
+      l_errorDescripcion : 'La descripción es requerida', //Variables con las validaciones de los errores principales de un form
+      l_errorCantidadTotal: 'La cantidad total es requerida',
+      l_errorPrecio: 'El precio es requerido',
+      l_errorCategorias: 'Por favor seleccione una categoría.',
+      l_errorUnidadMedida: 'Por favor seleccione una unidad de medida.',
     };
 
     modelo.articulos = await Articulos.find();
