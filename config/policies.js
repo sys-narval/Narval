f2327be718e119bbf84d2b6f620cdd4f7a9d2a5e
antonio17971 <jose.quesada.espinoza@est.una.ac.cr>
2018-10-24 17:00:00 -0600

    color

--- config/policies.js
+++ config/policies.js
@@ -11,11 +11,11 @@
 module.exports.policies = {
 
 
-  /*'*': 'is-logged-in',
+  '*': 'is-logged-in',
 
   // Bypass the `is-logged-in` policy for:
   'entrance/*': true,
   'account/logout': true,
   'view-homepage-or-redirect': true,
-  'deliver-contact-form-message': true,*/
+  'deliver-contact-form-message': true,
 };
