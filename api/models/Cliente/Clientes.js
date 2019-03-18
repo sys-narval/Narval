3449770df56a805685fd54a22fe2b5490c4b6091
adriangc15 <adriangerardo15@gmail.com>
2019-03-18 17:00:22 -0600

    Create Clientes.js
    Creacion de la primer version del modelo para clientes

---
+++ api/models/Cliente/Clientes.js
@@ -0,0 +1,59 @@
+/**
+ * Cliente/Clientes.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    nombre: {
+      type: "string",
+      maxLength: 20,
+      required: true,
+      unique: true,
+      description: "Nombre del cliente"
+    },
+
+    cedula: {
+      type: "string",
+      maxLength: 10,
+      description: "Cedula del cliente, se deja de 10 campos en caso de ced. jurídica"
+    },
+
+    correo: {
+      type: "string",
+      maxLength: 200,
+      isEmail: true,
+      description: "Correo electrónico del cliente"
+    },
+
+    telefono: {
+      type: "string",
+      maxLength: 10,
+      description: "Número telefónico del cliente"
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    contactos:{
+      collection: "Contactos",
+      via: "cliente"
+    }
+
+  },
+
+};
+
