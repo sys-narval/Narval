fb862275fc2e6505f68526e70c07f508fc5c1d08
leito2223 <41761553+leito2223@users.noreply.github.com>
2019-04-03 13:05:22 -0600

    revert2

--- api/controllers/clientes/view-clientes.js
+++ api/controllers/clientes/view-clientes.js
@@ -17,43 +17,15 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-
     let modelo = {
-      contactos: [{
-          empresa: 'Coca-Cola',
-          telefonoEmpresarial: 2555555,
-          correoEmpresarial: 'coca@gmail.com',
-          Contacto: [{
-              nombre: 'Jose quesada',
-              telefono: 88989899,
-              correo: 'jose@gmail.com'
-            },
-            {
-              nombre: 'Mario Porras',
-              telefono: 9998555,
-              correo: 'mario@gmail.com'
-            }
-          ]
-        },
-        {
-          empresa: 'Imprerial',
-          telefonoEmpresarial: 2555555,
-          correoEmpresarial: 'imperial@gmail.com',
-          Contacto: [{
-              nombre: 'Carlos quesada',
-              telefono: 88989899,
-              correo: 'carlos@gmail.com'
-            },
-            {
-              nombre: 'Juan Porras',
-              telefono: 9998555,
-              correo: 'juan@gmail.com'
-            }
-          ]
-        }
-      ]
+      l_errorNombre: 'El Nombre es requerido',
+      l_errorCorreo: 'El Correo es requerido',
+      l_errorCedula: 'El Cedula es requerido',
+      l_errorTelefono: 'El Telefono es requerido',
+      l_errorIdRepetido: 'La cedula ya existe',
     }
     // Respond with view.
+<<<<<<< HEAD
 <<<<<<< HEAD
     return exits.success();
     modelo
@@ -61,6 +33,10 @@ module.exports = {
     modelo.clientes=Cloud.extraerClientes();
     return exits.success({modelo});
 >>>>>>> parent of 53c44a9... Revert "Merge branch 'dev' of https://github.com/sys-narval/Narval into dev"
+=======
+    modelo.clientes=await Clientes.find();
+    return exits.success({modelo});
+>>>>>>> parent of 0621802... Revert "Merge branch 'dev' of https://github.com/sys-narval/Narval into dev"
   }
 
 
