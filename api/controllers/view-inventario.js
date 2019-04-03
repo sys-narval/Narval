97af92aae6214d6ca833596d40304274d4c9ca73
adriangc15 <adrian.chavarria.alvarez@est.una.ac.cr>
2019-04-02 19:30:21 -0600

    Cambios menores en funcion de extraer inventario

--- api/controllers/view-inventario.js
+++ api/controllers/view-inventario.js
@@ -30,7 +30,6 @@ module.exports = {
       l_errorIdRepetido: 'Artículo con el mismo ID ya existe',
     };
 
-    modelo.articulos = await Articulos.find();
     return exits.success({
       modelo
     });
