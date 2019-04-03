248f7837b46895580b2d68bd4b14c2e0added24a
antonio17971 <antonio17971>
2019-04-02 18:48:10 -0600

    pruebas de cloud

--- api/controllers/inventario/extraer-inventario.js
+++ api/controllers/inventario/extraer-inventario.js
@@ -18,7 +18,7 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    let articulos = await Articulos.find().log(); //Retorna todo de la base de datos local
+    let articulos = await Articulos.find(); //Retorna todo de la base de datos local
 
     return exits.success({
       articulos
