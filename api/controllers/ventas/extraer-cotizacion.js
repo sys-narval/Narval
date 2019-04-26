79cc84dd07d2377aab3689215fb3d610a08509e5
adriangc15 <adriangerardo15@gmail.com>
2019-04-26 14:50:44 -0600

    Creacion metodo para buscar una cotizacion en base al ID

--- api/controllers/ventas/extraer-cotizacion.js
+++ api/controllers/ventas/extraer-cotizacion.js
@@ -12,7 +12,7 @@ module.exports = {
       type: "string",
       description: "Nombre de la persona encargada"
     },
-    
+
     id: {
       type: "number",
       description: "ID de la cotización a extraer (en caso de conocerlo)"
@@ -22,16 +22,35 @@ module.exports = {
 
 
   exits: {
-
+    cotizacionNoEncontrada: {
+      message: "No se encuentra la cotización solicitado",
+      responseType: "notFound"
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
 
-    // All done.
-    return;
+    try {
+
+      let cotizacion = await Cotizaciones.findOne({
+          id: inputs.id
+        })
+        .populate("encargado")
+        .populate("cliente")
+        .populate("contacto")
+        .populate("articulos");
+
+      if (cotizacion === undefined) {
+        return exits.cotizacionNoEncontrada(`Cotización ${inputs.id} no encontrada`);
+      } else {
+        return exits.success(cotizacion);
+      }
+
+    } catch (error) {
+      return exits.error(error.message);
+    }
 
   }
-
 
 };
