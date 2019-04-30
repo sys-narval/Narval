5df3a044959dd7972a521c4c5bc775366b96e706
adriangc15 <adriangerardo15@gmail.com>
2019-04-29 21:41:22 -0600

    Agregando validaciones a la hora de guardar una cotizacion

--- api/controllers/ventas/insertar-cotizacion.js
+++ api/controllers/ventas/insertar-cotizacion.js
@@ -58,9 +58,26 @@ module.exports = {
       description: "Fecha del desmontaje del evento"
     },
 
+    estado: {
+      type: "string",
+      isIn: ["Activo", "Cancelado", "Pendiente", "Finalizado"],
+      defaultsTo: "Pendiente",
+      description: "Estado actual de la cotización, unicamente permitidos los mencionados anteriormente"
+    },
+
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    jsonArticulos: {
+      type: 'json',
+      description: `JSON con los artículos requeridos para la cotización (en caso de ser tipo alquiler o montaje).
+                        Se espera un JSON con la estructura { articulos: [ { id, cantidad, precio? } ] } Donde el precio es opcional
+                        en caso de no incluirse, se asigna el precio que tiene el articulo en la BD`
+    },
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
     encargado: {
       type: "number",
       required: true,
@@ -78,10 +95,6 @@ module.exports = {
       required: true,
       description: "ID del contacto del evento"
     },
-
-    jsonArticulos: {
-      type: "json"
-    }
 
   },
 
@@ -115,8 +128,50 @@ module.exports = {
        */
       if ((inputs.esMontaje || inputs.esAlquiler) && inputs.jsonArticulos === undefined) {
         return exits.error("En caso de Alquiler o Montaje, por favor ingrese artículos");
-      } else {
-        inputs.articulos = inputs.jsonArticulos.articulos.map(articulo => articulo.id);
+      }
+      /**
+       * En caso de montaje o alquiler y trae inventario
+       * normalizamos el inventario recibido
+       */
+      else if (inputs.esMontaje || inputs.esAlquiler) {
+
+        /**
+         * Validamos si todos los id de artículos existen en la BD
+         */
+        let idArticuloError;
+        let dbArticulos = await Articulos.find();
+
+        if (inputs.jsonArticulos.articulos.some((articulo) => {
+            let result = _.find(dbArticulos, (dbArticulo) => {
+              idArticuloError = articulo.id;
+              return dbArticulo.id === articulo.id;
+            });
+            return (result === undefined);
+          })) {
+          return exits.error(`El articulo con el Id ${idArticuloError} no existe en el inventario`);
+        }
+
+        /**
+         * Validamos los precios 
+         * en caso de no tener se le asigna el original del la BD
+         */
+
+        inputs.jsonArticulos.articulos.forEach((articulo, index) => {
+          if (articulo.precio === undefined) {
+            inputs.jsonArticulos.articulos[index].precio = _.find(dbArticulos, (dbArticulo) => {
+              idArticuloError = articulo.id;
+              return dbArticulo.id === articulo.id;
+            }).precio;
+          }
+        });
+      }
+
+      /**
+       * Validamos el caso en que solo es diseño
+       * no puede tener articulos
+       */
+      if (inputs.esDiseno && !(inputs.esMontaje || inputs.esAlquiler) && inputs.jsonArticulos !== undefined) {
+        return exits.error('En caso que es solo servicio de diseño por favor no ingresar articulos.');
       }
 
 
