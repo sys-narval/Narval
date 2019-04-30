f237d09c05b099196210dc9e87ca3482a73ff74d
adriangc15 <adriangerardo15@gmail.com>
2019-04-29 22:39:16 -0600

    Arreglo accion extraer cotizaciones

--- api/controllers/ventas/extraer-cotizaciones.js
+++ api/controllers/ventas/extraer-cotizaciones.js
@@ -27,14 +27,29 @@ module.exports = {
       let cotizaciones = await Cotizaciones.find()
         .populate("encargado")
         .populate("cliente")
-        .populate("contacto")
-        .populate("articulos");
+        .populate("contacto");
 
       if (cotizaciones === undefined) {
         return exits.noHayCotizaciones(`No existen cotizaciones.`);
-      } else {
-        return exits.success(cotizaciones)
       }
+
+      for (const index in cotizaciones) {
+        let cotizacion = cotizaciones[index];
+
+        if (cotizacion.esMontaje || cotizacion.esAlquiler) {
+          let dbArticulos = await Articulos.find({
+            where: {
+              id: {
+                in: cotizacion.articulos.map(articulo => articulo.id)
+              }
+            },
+            select: ["id", "descripcion", "categoria", "unidadMedida"]
+          });
+          cotizacion.articulos = cotizacion.articulos.map((articulo, index) => Object.assign(articulo, dbArticulos[index]));
+        }
+      }
+
+      return exits.success(cotizaciones);
 
     } catch (error) {
 
