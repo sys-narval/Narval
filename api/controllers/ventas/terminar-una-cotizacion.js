b1d1e73aa6ebe53e57b15d21a09d0ddc08de75c2
adriangc15 <adriangerardo15@gmail.com>
2019-05-21 20:24:34 -0600

    Implementando accion de terminar una cotizacion

--- api/controllers/ventas/terminar-una-cotizacion.js
+++ api/controllers/ventas/terminar-una-cotizacion.js
@@ -11,10 +11,11 @@ module.exports = {
 
     id: {
       type: "number",
+      required: true,
       description: "ID de la cotización a reservar los artículos"
     },
 
-    mensaje: {
+    comentario: {
       type: "string",
       description: "Mensaje de finalización para la cotización"
     }
@@ -23,14 +24,83 @@ module.exports = {
 
 
   exits: {
-
+    cotizacionNoEncontrada: {
+      message: "No se encuentra la cotización solicitada",
+      responseType: "notFound"
+    }
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
 
-    // All done.
-    return;
+    try {
+
+      let cotizacion = await Cotizaciones.findOne({
+        where: {
+          id: inputs.id
+        },
+        select: ["estado", "esMontaje", "esAlquiler", "articulos"]
+      });
+
+      // Comprobamos si la cotización existe en la BD
+      if (cotizacion === undefined) {
+        return exits.cotizacionNoEncontrada(`No se encuentra la cotización con el ID: ${inputs.id}`);
+      }
+
+      switch (cotizacion.estado) {
+        case "Activo":
+          // Verificamos si la cotizacion es alquiler o montaje
+          if (cotizacion.esMontaje || cotizacion.esAlquiler) {
+            let dbArticulos = await Articulos.find({
+              where: {
+                id: {
+                  in: cotizacion.articulos.map(articulo => articulo.id)
+                }
+              },
+              select: ["id", "cantidadLibre", "cantidadUso"]
+            });
+
+            cotizacion.articulos = cotizacion.articulos.map((articulo, index) => Object.assign(articulo, dbArticulos[index]));
+
+            for (const articulo of cotizacion.articulos) {
+              await Articulos.update({
+                id: articulo.id
+              }, {
+                cantidadLibre: (articulo.cantidadLibre + articulo.cantidad),
+                cantidadUso: (articulo.cantidadUso - articulo.cantidad)
+              });
+            }
+          }
+
+          // Si el evento estaba en estado 'Activo' y se termina
+          // se considera como 'Finalizado'
+          cotizacion.estado = "Finalizado";
+          break;
+
+        case "Pendiente":
+          // Si el evento estaba en estado 'Pendiente' y se termina
+          // se considera como 'Cancelado'
+          cotizacion.estado = "Cancelado";
+          break;
+        default:
+          
+          return exits.error(`No se puede Finalizar un evento en estado: ${cotizacion.estado}`);
+      }
+
+      cotizacion.comentario = inputs.comentario;
+
+      await Cotizaciones.update({
+        id: inputs.id
+      }, {
+        estado: cotizacion.estado,
+        comentario: cotizacion.comentario
+      });
+
+      return exits.success();
+
+    } catch (error) {
+      return exits.error(error.message);
+    }
 
   }
 
