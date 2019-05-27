e489a21b17a588282e53b459e5d2b6faf5f651be
adriangc15 <adriangerardo15@gmail.com>
2019-05-27 16:37:06 -0600

    Exportando funciones de excel4node al frontend

--- api/controllers/ventas/view-cotizaciones.js
+++ api/controllers/ventas/view-cotizaciones.js
@@ -15,44 +15,15 @@ module.exports = {
 
   },
 
-
   fn: async function (inputs, exits) {
 
-    let modelo = {
-    /*  cotizaciones: [{
-          id: 1,
-          encargado: 'Adrian Chavarria',
-          cliente: 'Coca-Cola',
-          ubicacion: 'Tamarindo, Guanacaste',
-          fechaMontaje: '2018-10-01',
-          fechaEvento: '2018-10-01',
-          fechaFin: '2018-12-05',
-          descripcion: 'Evento de CocaCola en playa tamarindo',
-          materiales: [{
-            codigo: '1',
-            descripcion: 'Tross 2m',
-            cantidad: '20'
-          }]
-        },
-        {
-          id: 2,
-          encargado: 'Antonio Quesada',
-          cliente: 'Imperial',
-          ubicacion: 'Puerto Viejo, Limon',
-          fechaMontaje: '2018-09-30',
-          fechaEvento: '2018-10-10',
-          fechaFin: '2018-12-05',
-          descripcion: 'Fiesta Imperial'
-        }
-      ]*/
-    };
+    let excel = require("excel4node");
 
     // Respond with view.
     return exits.success({
-      modelo
+      excel,
     });
 
   }
-
 
 };
